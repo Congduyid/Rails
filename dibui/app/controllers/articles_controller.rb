8e8dcb712bf741d52846fa879d05403de34bed8e
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-05-17 11:42:53 +0700

    them edit

--- dibui/app/controllers/articles_controller.rb
+++ dibui/app/controllers/articles_controller.rb
@@ -9,12 +9,17 @@ class ArticlesController < ApplicationController
 
 def create
 	@article = Article.new(params.require(:article).permit(:title, :text))
-   @article.save
-  redirect_to @article
+	@article.save
+	redirect_to @article
 end
 
 def show
     @article = Article.find(params[:id])
-  end
+end
+
+
+def edit
+  @article = Article.find(params[:id])
+end
 
 end
