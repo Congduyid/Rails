df61d7c119c609848ddce50479b3fda79faa369f
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-05-22 16:37:00 +0700

    Tao nhanh

--- dibui/app/models/article.rb
+++ dibui/app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
-	 validates :title, presence: {message: "Ko dc de trong"},length: { minimum: 5, too_short: "qua ngan"}, acceptance: {message: "Check do"}, uniqueness: true
-end
+	 validates :title, presence: {message: "Ko dc de trong"},length: { minimum: 5, too_short: "qua ngan"}, uniqueness: true
+	end
